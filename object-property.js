99b0032c1a03c4be1c3e555edbc1bc2574a8c625
abir246 <abir246n@gmail.com>
2020-04-07 00:59:29 +0600

    object-property

---
+++ object-property.js
@@ -0,0 +1,11 @@
+const students = [
+    {id: 1, name: 'Omar sunny'},
+    {id: 2, name: 'Maannaaaaaa'},
+    {id: 3, name: 'Moyooooooori'},
+    {id: 4, name: 'DeepJol'}
+
+];
+
+const names = students.map(s => s.name);
+const ids = students.map(s => s.id);
+console.log(ids);
