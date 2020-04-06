ff5a246e0a6a538a74078ed7dc97966eb93c49ba
abir246 <abir246n@gmail.com>
2020-04-06 23:45:48 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1, num2);
+    return
+}
+function add(num1, num2){
+    console.log(num1, num2)
+}
+const premik = {name:"smart dude", phone:76543};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [6, 5, 4]
+console.log(ages[11]);
