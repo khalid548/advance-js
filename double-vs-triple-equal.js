46c85bc6fb577789f03c0149fd75c93d438db9d0
abir246 <abir246n@gmail.com>
2020-04-06 23:59:08 +0600

    (==)&(===)

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,8 @@
+const first = 3;
+const second = "3";//only check value when(==)and value & type when (===)
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
